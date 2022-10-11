3268b5673f54f480e54c004b6030f7e3add2e35c
sujondev <emd26521@gmail.com>
2022-10-11 23:48:47 +0600

    adding option section

--- src/componets/Question/Question.js
+++ src/componets/Question/Question.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Question.css'
 
 const Question = ({ option }) => {
-    const { question } = option
+    const { question, options } = option
     return (
-        <div>
-            <h1>{question}</h1>
+        <div className='container shadow border rounded mb-4 p-4'>
+            <div>
+                <h4 className='text-primary'>{question}</h4>
+            </div>
+            <div className='row'>
+                {
+                    options.map(quizOption => <div className='option quiz-hover grid-col-6 fw-bold border p-3 rounded mt-2'>{quizOption}</div>)
+                }
+            </div>
         </div>
     );
 };
--- src/componets/QuizTopic/QuizTopic.js
+++ src/componets/QuizTopic/QuizTopic.js
@@ -4,11 +4,12 @@ import Question from '../Question/Question';
 
 const QuizTopic = () => {
     const data = useLoaderData()
+    const name = data.data
     const Options = data.data.questions
-    console.log(Options)
+    console.log(name)
     return (
         <div>
-            <h3>this quiz topic compoents</h3>
+            <h3 className='text-center mt-5 mb-4 fw-bold text-primary'>Quiz of {name.name}</h3>
             {
                 Options.map(option => <Question key={option.id} option={option}></Question>)
             }
