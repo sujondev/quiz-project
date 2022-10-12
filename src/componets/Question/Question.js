52ab0626c9ff74d6e92f6ad7d4d38f302a2c07b4
sujondev <emd26521@gmail.com>
2022-10-12 22:24:37 +0600

    corret answer showing

--- src/componets/Question/Question.js
+++ src/componets/Question/Question.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuizOption from '../QuizOption/QuizOption';
 import { EyeIcon } from '@heroicons/react/24/solid'
+import { click } from '@testing-library/user-event/dist/click';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Question = ({ option }) => {
     const { question, options, correctAnswer } = option
+    const handleEyeIcon = () => {
+        toast.success(correctAnswer, {
+            position: "top-center",
+            autoClose: 1000,
+            theme: "colored"
+        })
+    }
 
     return (
         <div className='container shadow border rounded mb-4 p-4'>
             {<div className='d-flex justify-content-between'>
                 <h4 className='text-primary'>{question}</h4>
                 <div>
-                    <EyeIcon style={{ width: '20px', height: '20px' }} />
+                    <EyeIcon onClick={handleEyeIcon} style={{ width: '20px', height: '20px' }} />
                 </div>
             </div>}
             < div className='row' >
@@ -18,6 +27,7 @@ const Question = ({ option }) => {
                     options.map((quizOption, index) => <QuizOption key={index} quizOption={quizOption} correctAnswer={correctAnswer}></QuizOption>)
                 }
             </div >
+            <ToastContainer />
         </div >
     );
 };
