18e7d90d75b989fca101bef6c51cd66e51d39466
sujondev <emd26521@gmail.com>
2022-10-12 17:54:12 +0600

    adding quiz answer and wrong answer toast

--- src/componets/QuizOption/QuizOption.js
+++ src/componets/QuizOption/QuizOption.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import './QuizOption.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QuizOption = ({ quizOption, correctAnswer }) => {
     const handleQuiz = () => {
         if (quizOption === correctAnswer) {
-
+            toast.success("Answer is right!", {
+                position: "top-center",
+                autoClose: 1000,
+                theme: "colored"
+            });
+        }
+        else {
+            toast.error("Answer is wrong!", {
+                position: "top-center",
+                autoClose: 1000,
+                theme: "colored"
+            });
         }
     }
 
     return (
         <div onClick={handleQuiz} className='option quiz-hover col-6 fw-bold border p-3 rounded mt-2'>
             {quizOption}
+            <ToastContainer />
         </div>
+
     );
 };
 
